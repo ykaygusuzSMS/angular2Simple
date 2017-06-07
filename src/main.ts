a4aec8adcb09d391ff5935563f89eca44e42b048
ykaygusuzSMS <yakup.kaygusuz@smsmt.com>
2017-06-07 11:44:49 +1000

    Added Prod Mode

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 //import './libs';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
-
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
 //document.write('<my-app></my-app>');
